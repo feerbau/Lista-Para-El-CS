548ae1dae440dc36476ec324f00e7e4240b5ac10
feerbau <fermin-lp@hotmail.com>
2021-03-31 16:52:29 -0300

    Fix jeje

--- bot.js
+++ bot.js
@@ -11,7 +11,7 @@ function exists_group(id){
 
 function add_to_list(ctx,name, list){
     if (list.length > 4)
-        ctx.reply('Ya esta llena la lista, dormiste.')
+        return ctx.reply('Ya esta llena la lista, dormiste.')
     list.push(name)
 }
 bot_listas.start((ctx) => {
@@ -23,9 +23,9 @@ bot_listas.command('toy', (ctx) => {
     exists_group(ctx.chat.id)
     if (!listas[ctx.chat.id].includes(ctx.from.first_name)){    
         add_to_list(ctx,ctx.from.first_name,listas[ctx.chat.id])
-        ctx.reply('Adentro '+ctx.from.first_name)
+        return ctx.reply('Adentro '+ctx.from.first_name)
     }else{
-        ctx.reply('Ya estas en la lista pa.')
+        return ctx.reply('Ya estas en la lista pa.')
     }
     })
 
@@ -39,9 +39,9 @@ bot_listas.command('salir', (ctx) => {
     let index = listas[ctx.chat.id].indexOf(ctx.from.first_name)
     if (index > -1) {
         listas[ctx.chat.id].splice(index, 1);
-        ctx.reply('Sos tremendo gil, chau.');
+        return ctx.reply('Sos tremendo gil, chau.');
     }else{
-        ctx.reply('De donde queres salir vos, banana.');
+        return ctx.reply('De donde queres salir vos, banana.');
     }
 })
 
@@ -51,7 +51,7 @@ bot_listas.command('lista', (ctx)=>{
 })
 
 bot_listas.command('ayuda',(ctx)=>{
-    ctx.reply('Bote es el dios, el unico. El inigualable. \nSi queres ayuda, Bote es el unico que puede dartela.\n Larga vida al dios Bote')
+    return ctx.reply('Bote es el dios, el unico. El inigualable. \nSi queres ayuda, Bote es el unico que puede dartela.\n Larga vida al dios Bote')
 })
 function list(lista){
     return lista.map((u,i) => ++i+". "+u+'\n')
