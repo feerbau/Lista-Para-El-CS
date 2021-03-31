e3e4230f1d963db93c7a631271650ee6469bc7fd
AZCONA, MARCOS <marcosazcona2000@hotmail.com>
2021-03-31 17:58:32 -0300

    Agregado todo las pelotudeces que me pidio nacho

--- bot.js
+++ bot.js
@@ -5,25 +5,53 @@ const bot_listas = new Telegraf('1752245041:AAGTaDOnPG5ndTYAUnQISoSxJAPQ0aRNfkc'
 var listas = {};
 
 function exists_group(id){
-    if (!listas[id])
-        listas[id] = []
-        /*
-            listas {
-                'activos':
-                'suplentes':
-            }
-        */
+    if (!listas[id]){
+        // If not exist an grup, create it empty.
+        listas[id] = {}
+        listas["activos"] = []
+        listas["suplentes"] = []
+    }
 }
 
 function add_to_list(ctx,name, list){
-    let feedback = ""
     if (list.length >= 5){
-        printList(ctx)
-        return ctx.reply('Ya esta llena la lista, dormiste.')
+        printAll(ctx)
+        listas["suplentes"].push(name)
+        return ctx.reply('Ya esta llena la lista, dormiste. Entras como suplente')
     }
-    list.push(name)
-    return ctx.reply('Adentro '+ctx.from.first_name)
+    //list.push(name)
+    list["activos"].push(name)
+    return ctx.reply('Adentro ' + ctx.from.first_name)
 }
+
+function getFirstSustitute(chatId){
+    return listas[chatId]["suplentes"].shift()
+}
+
+function list(lista){
+    let nuevaLista = ""
+    lista.map((u,i) => nuevaLista += `${++i}. ${u} \n`)
+    return nuevaLista
+}
+
+function printList(ctx,typeOfPlayer){
+    exists_group(ctx.chat.id);
+    return list(listas[ctx.chat.id][typeOfPlayer])
+}
+
+function printAll(ctx){
+    if (listas[ctx.chat.id]["activos"].length == 0){
+        return ctx.reply("No hay nadie")
+    }
+    let listado = "Listado activos: \n"
+    listado += `${printList(ctx,"activos")} \n`
+    if (listas[ctx.chat.id]["suplentes"].length > 0){
+        listado += "Listado suplentes: \n"
+        listado += `${printList(ctx,"suplentes")} \n`
+    }
+    return ctx.reply(listado)
+}
+
 bot_listas.start((ctx) => {
     ctx.reply('PASAME TU LISTITA PA');
     listas[ctx.chat.id] = []
@@ -31,11 +59,11 @@ bot_listas.start((ctx) => {
 
 bot_listas.command('toy', (ctx) => {
     exists_group(ctx.chat.id)
-    if (!listas[ctx.chat.id].includes(ctx.from.first_name)){    
+    if (!listas[ctx.chat.id]["activos"].includes(ctx.from.first_name)){    
         add_to_list(ctx,ctx.from.first_name,listas[ctx.chat.id])
-    }else{
-        return ctx.reply('Ya estas en la lista pa.')
+        return
     }
+    return ctx.reply('Ya estas en la lista pa.')
 })
 
 bot_listas.command('limpiar', (ctx) =>{
@@ -43,39 +71,30 @@ bot_listas.command('limpiar', (ctx) =>{
     listas[ctx.chat.id] = []
     ctx.reply('Limpiada padre!')
 })
+
 bot_listas.command('salir', (ctx) => {
     exists_group(ctx.chat.id)
     let index = listas[ctx.chat.id].indexOf(ctx.from.first_name)
     if (index > -1) {
-        listas[ctx.chat.id].splice(index, 1);
+        // Exist the user. Then remove it
+        listas[ctx.chat.id]["activos"].splice(index, 1);
+        if (listas[ctx.chat.id]["suplentes"].length > 0){
+            // Add the first suplent in the actives players list
+            listas[ctx.chat.id]["activos"].push(getFirstSustitute())
+        }
         return ctx.reply('Sos tremendo gil, chau.');
-    }else{
-        return ctx.reply('De donde queres salir vos, banana.');
     }
+
+    // Don't exist that user
+    return ctx.reply('De donde queres salir vos, banana.');  
 })
 
-function list(lista){
-    let nuevaLista = ""
-    lista.map((u,i) => nuevaLista += `${++i}. ${u} \n`)
-    return nuevaLista
-    //return lista.map((u,i) => ++i+". "+u+'\n')
-}
-
-function printList(ctx){
-    exists_group(ctx.chat.id);
-    return (listas[ctx.chat.id] && listas[ctx.chat.id].length > 0) ? ctx.reply("La lista es \n"+list(listas[ctx.chat.id])): ctx.reply("No hay nadie")
-}
-
 bot_listas.command('lista', (ctx)=>{
-    /*
-    exists_group(ctx.chat.id);
-    (listas[ctx.chat.id] && listas[ctx.chat.id].length > 0) ? ctx.reply("La lista es \n"+list(listas[ctx.chat.id])): ctx.reply("No hay nadie")
-    */
-   printList(ctx)
+   printAll(ctx)
 })
 
 bot_listas.command('ayuda',(ctx)=>{
-    return ctx.reply('Bote es el dios, el unico. El inigualable. \nSi queres ayuda, Bote es el unico que puede dartela.\n Larga vida al dios Bote')
+    return ctx.reply('Fer,colgate de esta.')
 })
 
 bot_listas.launch()
