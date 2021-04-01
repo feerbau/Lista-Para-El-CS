f0773db458aeee06f037388f6e36fa9932d125d4
Agustin Alberca <aa.fi.unlp@gmail.com>
2021-04-01 13:11:21 -0300

    test a lo tonto

--- bot.js
+++ bot.js
@@ -116,6 +116,7 @@ bot_listas.command(['ayuda','help','comandos'],(ctx)=>{
 })
 
 bot_listas.command('hora',(ctx)=>{
+    ctx.reply(`${ctx}`)
     let hora = listas[id]["hora_activos"] = ctx.update.message.text.split(' ').splice(1)
     if (hora != nil){
         listas[id]["hora_activos"] = hora
